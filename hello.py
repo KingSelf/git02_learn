0b278a626f44d82ea80bcb89fdea974ccaed2df3
zk_mac_test <519715203@qq.com>
2019-03-19 21:44:40 +0800

    add hello file

---
+++ hello.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ==============================================================================
+#   Copyright (C) 2019 MyCompany. All rights reserved.
+#   
+#   File Name  : hello.py
+#   Author     : Zhaokui
+#   Create Date: 2019-03-19 21:43
+#   Describle  : 
+#
+# ==============================================================================
+
+
+def main():
+    print("hello!")
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
